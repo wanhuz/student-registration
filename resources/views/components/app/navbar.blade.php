b0b83d5a54eb7bafc9c3144529a5d21143cfa5e1
wan <wanhuz96@gmail.com>
2024-10-13 10:14:59 +0800

    fix navbar to be sticky

--- resources/views/components/app/navbar.blade.php
+++ resources/views/components/app/navbar.blade.php
@@ -1,15 +1,13 @@
-<nav class="navbar navbar-light bg-light p-3">
+<nav class="navbar navbar-light bg-light p-3 position-sticky">
     <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
             <x-application-logo class="w-25"></x-application-logo>
         </a>
         <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     </div>
-    <div class="col-12 col-md-4 col-lg-2">
-        <input class="form-control form-control-dark" type="text" placeholder="Search" aria-label="Search">
-    </div>
+
     <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
         <div class="dropdown">
             <button class="btn  dropdown-toggle mt-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
